fbe8429d677b30ee6d4089ebe601299f5e85cd00
HuhuliaDiana <dianahuhulia@gmail.com>
2021-03-04 17:25:59 +0200

    third commit

--- cts_seminar2/src/ro/ase/cts/clase/readers/AngajatReader.java
+++ cts_seminar2/src/ro/ase/cts/clase/readers/AngajatReader.java
@@ -1,5 +1,48 @@
 package ro.ase.cts.clase.readers;
 
-public class AngajatReader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+
+public class AngajatReader extends AplicantReader {
+
+
+
+public AngajatReader(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+
+public List<Aplicant> readAplicanti() throws FileNotFoundException {	//dip-foloseste tipul abstract List, nu ArrayList, si Aplicant , nu angajat
+
+	Scanner input2 = new Scanner(new File(super.fileName));
+	input2.useDelimiter(",");
+	List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+	while (input2.hasNext()) {
+		String nume = input2.next();
+		String prenume = input2.next();
+		int varsta = input2.nextInt();
+		int punctaj = input2.nextInt();
+		int nr = input2.nextInt();
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input2.next();
+		int salariu = input2.nextInt();
+		String ocupatie = input2.next();
+		Angajat a = new Angajat(nume, prenume, varsta, punctaj, nr, vect, salariu, ocupatie);
+		angajati.add(a);
+	}
+	input2.close();
+	return angajati;
+}
+
+
+
 
 }
