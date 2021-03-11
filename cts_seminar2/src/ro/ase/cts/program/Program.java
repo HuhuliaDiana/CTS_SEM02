df4f772f0ec0795738b4e173d1415f9a963835b9
HuhuliaDiana <dianahuhulia@gmail.com>
2021-03-11 16:28:57 +0200

    second commit sem 3

--- cts_seminar2/src/ro/ase/cts/program/Program.java
+++ cts_seminar2/src/ro/ase/cts/program/Program.java
@@ -18,7 +18,10 @@ public class Program {
 		try {
 			listaAngajati = citesteAplicanti(new AngajatReader("angajati.txt"));
 			for(Aplicant angajat:listaAngajati)
+				{
 				System.out.println(angajat.toString());
+				System.out.print("Angajatul are finantarea " + angajat.getFinantare());
+				}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
