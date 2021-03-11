9b05dd825ea07f1040e836a90875008e5cbf50c2
HuhuliaDiana <dianahuhulia@gmail.com>
2021-03-11 16:09:52 +0200

    first commit sem 3

--- cts_seminar2/src/ro/ase/cts/clase/readers/AplicantReader.java
+++ cts_seminar2/src/ro/ase/cts/clase/readers/AplicantReader.java
@@ -2,18 +2,41 @@ package ro.ase.cts.clase.readers;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
 
 public abstract class AplicantReader {
-	protected String fileName;//ca sa aiba acces copiii lui apReader la el
-    public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+	protected String fileName;
+	
 	
 	public AplicantReader(String fileName) {
 		super();
 		this.fileName = fileName;
 	}
-
+	
+	 public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+	 
+	 public void readAplicant(Scanner input2, Aplicant aplicant) {
+		 
+		 String nume = input2.next();
+			String prenume = input2.next();
+			int varsta = input2.nextInt();
+			int punctaj = input2.nextInt();
+			int nr = input2.nextInt();
+			String[] vect = new String[5];
+			for (int i = 0; i < nr; i++)
+				vect[i] = input2.next();
+			
+			aplicant.setNume(nume);
+			aplicant.setPrenume(prenume);
+			aplicant.setPunctaj(punctaj);
+			aplicant.setVarsta(varsta);
+			aplicant.setDenumiriProiecte(nr,vect);
+			
+			
+	 }
+	
 	
 
 }
