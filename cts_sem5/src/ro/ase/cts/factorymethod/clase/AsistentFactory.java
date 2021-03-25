588ca0b67eb74ed1429aa0262d7dcdbc1dacfd14
HuhuliaDiana <dianahuhulia@gmail.com>
2021-03-25 17:14:12 +0200

    second commit factory method

---
+++ cts_sem5/src/ro/ase/cts/factorymethod/clase/AsistentFactory.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.factorymethod.clase;
+
+public class AsistentFactory implements FactoryPersonal {
+
+	@Override
+	public PersonalSpital getPersonal(String nume) {
+		// TODO Auto-generated method stub
+		return new Asistent(nume);
+	}
+	
+
+}
---
+++ cts_sem5/src/ro/ase/cts/factorymethod/program/Main.java
@@ -0,0 +1,26 @@
+package ro.ase.cts.factorymethod.program;
+
+import ro.ase.cts.factorymethod.clase.AsistentFactory;
+import ro.ase.cts.factorymethod.clase.FactoryPersonal;
+import ro.ase.cts.factorymethod.clase.InfirmierFactory;
+import ro.ase.cts.factorymethod.clase.MedicFactory;
+import ro.ase.cts.factorymethod.clase.PersonalSpital;
+
+public class Main {//Clientul
+
+	public static void printeazaPersonalSpital(FactoryPersonal factory, String nume) {
+		PersonalSpital personal=factory.getPersonal(nume);
+		System.out.println(personal.toString());
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		printeazaPersonalSpital(new MedicFactory(), "Maria");
+		printeazaPersonalSpital(new AsistentFactory(), "Ion");
+		printeazaPersonalSpital(new InfirmierFactory(), "Andrei");
+		
+
+	}
+
+}
